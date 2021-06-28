57908807d045a20a866f755d41233330bdca2d5b
Cray Elliott <MP2E@archlinux.us>
2021-06-28 05:02:10 -0700

    Add LLAPI support

--- GBA.sv
+++ GBA.sv
@@ -176,7 +176,6 @@ module emu
 
 assign ADC_BUS  = 'Z;
 assign {UART_RTS, UART_TXD, UART_DTR} = 0;
-assign USER_OUT = '1;
 
 assign AUDIO_S   = 1;
 assign AUDIO_MIX = status[8:7];
@@ -184,7 +183,7 @@ assign AUDIO_MIX = status[8:7];
 assign LED_USER  = cart_download | bk_pending;
 assign LED_DISK  = 0;
 assign LED_POWER = 0;
-assign BUTTONS   = 0;
+assign BUTTONS   = llapi_osd;
 assign VGA_SCALER= 0;
 
 assign {SD_SCK, SD_MOSI, SD_CS} = 'Z;
@@ -241,6 +240,7 @@ parameter CONF_STR = {
 	"O78,Stereo Mix,None,25%,50%,100%;",
 	"OK,Spritelimit,Off,On;",	 
 	"o23,Scale,Normal,V-Integer,Narrower HV-Integer,Wider HV-Integer;",
+        "OM,Serial Mode,Off,LLAPI;",
 	"O5,Pause when OSD is open,Off,On;",
 	"- ;",
 	"R0,Reset;",
@@ -276,6 +276,9 @@ wire [15:0] joy2;
 wire [15:0] joy_unmod;
 wire [10:0] ps2_key;
 
+wire [15:0] joy1_usb;
+wire [15:0] joy2_usb;
+
 wire [21:0] gamma_bus;
 wire [15:0] sdram_sz;
 
@@ -292,8 +295,8 @@ hps_io #(.STRLEN($size(CONF_STR)>>3), .WIDE(1)) hps_io
 	.buttons(buttons),
 	.forced_scandoubler(forced_scandoubler),
 
-	.joystick_0(joy1),
-	.joystick_1(joy2),
+	.joystick_0(joy1_usb),
+	.joystick_1(joy2_usb),
 	.ps2_key(ps2_key),
 
 	.status(status),
@@ -684,6 +687,151 @@ always @(posedge clk_sys) begin
 				if(cart_id == {"GUNSTAR FH", 12'h000} )        begin                                           sprite_quirk <= 1; end // Gunstar Super/Future Heroes J+EU+US
 			end
 		end
+	end
+end
+
+////////////////////////////  LLAPI  ///////////////////////////////////
+
+wire [31:0] llapi_buttons, llapi_buttons2;
+wire [71:0] llapi_analog, llapi_analog2;
+wire [7:0]  llapi_type, llapi_type2;
+wire llapi_en, llapi_en2;
+
+wire llapi_select = status[22];
+
+wire llapi_latch_o, llapi_latch_o2, llapi_data_o, llapi_data_o2;
+
+// Indexes:
+// 0 = D+    = P1 Latch
+// 1 = D-    = P1 Data
+// 2 = TX-   = LLAPI Enable
+// 3 = GND_d = N/C
+// 4 = RX+   = P2 Latch
+// 5 = RX-   = P2 Data
+
+always_comb begin
+	USER_OUT = 6'b111111;
+	if (llapi_select) begin
+		USER_OUT[0] = llapi_latch_o;
+		USER_OUT[1] = llapi_data_o;
+		USER_OUT[2] = ~(llapi_select & ~OSD_STATUS);
+		USER_OUT[4] = llapi_latch_o2;
+		USER_OUT[5] = llapi_data_o2;
+	end
+end
+
+LLAPI llapi
+(
+	.CLK_50M(CLK_50M),
+	.LLAPI_SYNC(vbl),
+	.IO_LATCH_IN(USER_IN[0]),
+	.IO_LATCH_OUT(llapi_latch_o),
+	.IO_DATA_IN(USER_IN[1]),
+	.IO_DATA_OUT(llapi_data_o),
+	.ENABLE(llapi_select & ~OSD_STATUS),
+	.LLAPI_BUTTONS(llapi_buttons),
+	.LLAPI_ANALOG(llapi_analog),
+	.LLAPI_TYPE(llapi_type),
+	.LLAPI_EN(llapi_en)
+);
+
+LLAPI llapi2
+(
+	.CLK_50M(CLK_50M),
+	.LLAPI_SYNC(vbl),
+	.IO_LATCH_IN(USER_IN[4]),
+	.IO_LATCH_OUT(llapi_latch_o2),
+	.IO_DATA_IN(USER_IN[5]),
+	.IO_DATA_OUT(llapi_data_o2),
+	.ENABLE(llapi_select & ~OSD_STATUS),
+	.LLAPI_BUTTONS(llapi_buttons2),
+	.LLAPI_ANALOG(llapi_analog2),
+	.LLAPI_TYPE(llapi_type2),
+	.LLAPI_EN(llapi_en2)
+);
+
+reg llapi_button_pressed, llapi_button_pressed2;
+
+always @(posedge CLK_50M) begin
+	if (reset) begin
+		llapi_button_pressed  <= 0;
+		llapi_button_pressed2 <= 0;
+	end else begin
+		if (|llapi_buttons)
+			llapi_button_pressed  <= 1;
+		if (|llapi_buttons2)
+			llapi_button_pressed2 <= 1;
+	end
+end
+
+// controller id is 0 if there is either an Atari controller or no controller
+// if id is 0, assume there is no controller until a button is pressed
+wire use_llapi = llapi_en && llapi_select && (|llapi_type || llapi_button_pressed);
+wire use_llapi2 = llapi_en2 && llapi_select && (|llapi_type2 || llapi_button_pressed2);
+
+// "J1,A,B,L,R,Select,Start,Turbo;",
+
+wire [15:0] joy_ll_a;
+always_comb begin
+	// map for saturn controller
+	// use L and R instead of top face buttons
+	// no select button so use Z
+	if (llapi_type == 3 || llapi_type == 8) begin
+		joy_ll_a = { 4'd0,
+			llapi_buttons[2],  llapi_buttons[3],                   // Rewind Fast-Forward
+			llapi_buttons[5],  llapi_buttons[6],                   // Start Select
+			llapi_buttons[9] | llapi_buttons[7], llapi_buttons[8], // R L
+			llapi_buttons[0],  llapi_buttons[1],                   // B A
+			llapi_buttons[27], llapi_buttons[26], llapi_buttons[25], llapi_buttons[24] // d-pad
+		};
+	end else begin
+		joy_ll_a = { 4'd0,
+			llapi_buttons[2],  llapi_buttons[3], // Rewind Fast-Forward
+			llapi_buttons[5],  llapi_buttons[4], // Start Select
+			llapi_buttons[7],  llapi_buttons[6], // RT LT
+			llapi_buttons[0],  llapi_buttons[1], // B A
+			llapi_buttons[27], llapi_buttons[26], llapi_buttons[25], llapi_buttons[24] // d-pad
+		};
+	end
+end
+
+wire [15:0] joy_ll_b;
+always_comb begin
+	// map for saturn controller
+	// use L and R instead of top face buttons
+	// no select button so use Z
+	if (llapi_type2 == 3 || llapi_type2 == 8) begin
+		joy_ll_b = { 4'd0,
+			llapi_buttons2[2],  llapi_buttons2[3],                    // Rewind Fast-Forward
+			llapi_buttons2[5],  llapi_buttons2[6],                    // Start Select
+			llapi_buttons2[9] | llapi_buttons2[7], llapi_buttons2[8], // R L
+			llapi_buttons2[0],  llapi_buttons2[1],                    // B A
+			llapi_buttons2[27], llapi_buttons2[26], llapi_buttons2[25], llapi_buttons2[24] // d-pad
+		};
+	end else begin
+		joy_ll_b = { 4'd0,
+			llapi_buttons2[2],  llapi_buttons2[3], // Rewind Fast-Forward
+			llapi_buttons2[5],  llapi_buttons2[4], // Start Select
+			llapi_buttons2[7],  llapi_buttons2[6], // RT LT
+			llapi_buttons2[0],  llapi_buttons2[1], // B A
+			llapi_buttons2[27], llapi_buttons2[26], llapi_buttons2[25], llapi_buttons2[24] // d-pad
+		};
+	end
+end
+
+wire llapi_osd = (llapi_buttons[26] && llapi_buttons[5] && llapi_buttons[0]) || (llapi_buttons2[26] && llapi_buttons2[5] && llapi_buttons2[0]);
+
+// if LLAPI is enabled, shift USB controllers to next available player slot
+always_comb begin
+	if (use_llapi && use_llapi2) begin
+		joy1 = joy_ll_a;
+		joy2 = joy_ll_b;
+	end else if (use_llapi || use_llapi2) begin
+		joy1 = use_llapi  ? joy_ll_a : joy1_usb;
+		joy2 = use_llapi2 ? joy_ll_b : joy1_usb;
+	end else begin
+		joy1 = joy1_usb;
+		joy2 = joy2_usb;
 	end
 end
 
